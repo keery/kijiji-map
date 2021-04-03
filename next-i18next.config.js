7c91043eaa8e813a5ff103e488cf361443723775
Guillaume Esnault <mon-compte1131@live.fr>
2021-04-03 16:35:59 +0200

    fix: set i18n locale path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     defaultLocale: 'fr',
     locales: ['fr', 'en'],
+    localePath: path.resolve('./public/static/locales'),
   },
   react: {
     useSuspense: false,

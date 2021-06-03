80fbd73c57a81e90e2e0e26453ce9d557f398035
Guillaume Esnault <mon-compte1131@live.fr>
2021-06-03 22:29:16 +0100

    fix: disable cors

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -3,8 +3,10 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   settings: {
     cors: {
-      enabled: isProd,
-      origin: isProd ? [process.env.FRONT_URL] : ["*"],
+      enabled: false,
+      origin: "*",
+      headers: "*",
+      // origin: isProd ? [process.env.FRONT_URL] : ["*"],
     },
   },
 };

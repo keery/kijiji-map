56f7cf6d1c0ecd5c135dd6e30621e61c76302379
Guillaume Esnault <mon-compte1131@live.fr>
2021-06-03 19:33:12 +0100

    chore: add BASE_URL config strapi

--- server/config/server.js
+++ server/config/server.js
@@ -1,12 +1,13 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   cron: {
     enabled: true,
   },
+  url: env("BASE_URL", "/"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '9e3df40dee2b9c9aa36404d215962f8d'),
+      secret: env("ADMIN_JWT_SECRET", "9e3df40dee2b9c9aa36404d215962f8d"),
     },
   },
-})
+});

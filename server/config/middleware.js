2d189e9c5ef95561001525cc06108592578f0440
Guillaume Esnault <mon-compte1131@live.fr>
2021-06-03 20:29:11 +0100

    chore: sets api cors

---
+++ server/config/middleware.js
@@ -0,0 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  settings: {
+    cors: {
+      enabled: isProd,
+      origin: isProd ? [process.env.FRONT] : ["*"],
+    },
+  },
+};

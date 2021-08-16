5c6e51be98d72d53e290a6ce4c44629cc9eb90ac
Keery <mon-compte1131@live.fr>
2021-08-16 15:09:37 -0400

    feat: number room filter will display all results with more rooms

--- client/utils/filters.ts
+++ client/utils/filters.ts
@@ -48,33 +48,24 @@ export const getDefaultValue = () => {
 }
 
 export const sizeMapping = {
-  1: [0],
-  2: [0],
-  3: [1, 1.5],
-  4: [2, 2.5],
-  5: [3, 3.5],
-  6: [4, 4.5],
-  7: [5],
+  1: 0,
+  2: 0,
+  3: 1,
+  4: 2,
+  5: 3,
+  6: 4,
+  7: 5,
 }
 
 export const getSize = (size: number = 0) => {
   if (Number(size) === 0) return null
 
-  const mappedSize = sizeMapping[size]
-
-  if (!mappedSize) {
+  if (!sizeMapping[size]) {
     return console.log('ERROR Mapping size')
   }
 
-  if (mappedSize.length === 1) {
-    return {
-      numberbedrooms_eq: mappedSize[0],
-    }
-  }
-
   return {
-    numberbedrooms_gte: mappedSize[0],
-    numberbedrooms_lte: mappedSize[1],
+    numberbedrooms_gte: sizeMapping[size],
   }
 }
 

a967cb704fc68816e02f58916ca8196390d41ed7
Guillaume <contact@guillaumeesnault.fr>
2021-09-16 12:00:01 -0400

    fix: ads skeleton in mobile

--- client/components/ListAdsSkeleton.tsx
+++ client/components/ListAdsSkeleton.tsx
@@ -1,58 +1,97 @@
 import React from 'react'
-import { Skeleton, VStack, Divider, Flex, HStack } from '@chakra-ui/react'
+import {
+  Skeleton,
+  VStack,
+  Divider,
+  Flex,
+  HStack,
+  AspectRatio,
+} from '@chakra-ui/react'
 import HomeIcon from 'public/assets/img/home.svg'
 
 const AdCardSkeleton = () => (
   <Flex
-    p={2.5}
     borderRadius="30px"
+    p={2.5}
     w="100%"
-    minH="220px"
+    pr={{ base: 2.5, sm: 4 }}
     boxShadow="0px 0px 7px #ccc"
   >
-    <Flex>
-      <Skeleton w="250px" h="200px" borderRadius="20px" />
-    </Flex>
     <Flex
-      pl={5}
-      py={1}
-      w="1px"
-      flex={1}
-      direction="column"
-      justifyContent="space-between"
+      w="100%"
+      className="adCard"
+      role="group"
+      direction={{ base: 'column', sm: 'row' }}
     >
-      <Flex h="100%" direction="column" justifyContent="space-between">
-        <Flex direction="column">
-          <Skeleton w="80%" h="25px" mb={2} />
-          <HStack justifyContent="flex-start">
-            <Skeleton w="30%" h="29px" />
-            <Skeleton w="30%" h="29px" />
-          </HStack>
-        </Flex>
-        <Flex justifyContent="space-between" alignItems="flex-end" pb={2}>
-          <Skeleton w="20%" h="14px" />
-          <Skeleton w="25%" h="30px" />
-        </Flex>
-      </Flex>
-      <HStack
-        justifyContent="space-between"
-        borderTop="1px solid"
-        borderColor="gray.100"
-        py={2}
-        px={1}
-        alignItems="center"
-        spacing={3}
+      <AspectRatio
+        borderRadius="20px"
+        overflow="hidden"
+        w={{ base: '100%', sm: '200px', md: '180px', lg: '250px' }}
       >
-        <Skeleton w="10%" h="20px" pb={1} />
-        <Divider orientation="vertical" h="85%" />
-        <Skeleton w="10%" h="20px" pb={1} />
-        <Divider orientation="vertical" h="85%" />
-        <Skeleton w="10%" h="20px" pb={1} />
-        <Divider orientation="vertical" h="85%" />
-        <Skeleton w="10%" h="20px" pb={1} />
-        <Divider orientation="vertical" h="85%" />
-        <Skeleton w="10%" h="20px" pb={1} />
-      </HStack>
+        <Skeleton />
+      </AspectRatio>
+
+      <Flex
+        py={1}
+        pr={{ base: 2, sm: 0 }}
+        pl={{ base: 2, sm: 3 }}
+        pt={{ base: 3, sm: 0 }}
+        w={{ base: '100%', sm: '1px' }}
+        flex={1}
+        direction="column"
+        justifyContent="space-between"
+      >
+        <Flex h="100%" direction="column" justifyContent="space-between">
+          <Flex direction="column">
+            {/* TITLE */}
+            <Skeleton w="80%" h="25px" mb={2} />
+            {/* TAGS */}
+            <HStack justifyContent="flex-start">
+              <Skeleton w="30%" h="29px" />
+              <Skeleton w="30%" h="29px" />
+            </HStack>
+          </Flex>
+          {/* DATE + PRICE */}
+          <Flex
+            justifyContent="space-between"
+            alignItems="flex-end"
+            pb={3}
+            pt={{ base: 2, sm: 6, lg: 0 }}
+          >
+            <Skeleton w="20%" h="14px" />
+            <Skeleton w="25%" h="30px" />
+          </Flex>
+        </Flex>
+        <HStack
+          justifyContent="space-evenly"
+          borderTop="1px solid"
+          borderColor="gray.100"
+          pt={2}
+          pb={{ base: 0, sm: 2 }}
+          px={1}
+          alignItems="center"
+          spacing={3}
+        >
+          <Skeleton w="10%" h="20px" pb={1} />
+          <Divider orientation="vertical" h="85%" />
+          <Skeleton w="10%" h="20px" pb={1} />
+          <Divider orientation="vertical" h="85%" />
+          <Skeleton w="10%" h="20px" pb={1} />
+          <Divider orientation="vertical" h="85%" />
+          <Skeleton w="10%" h="20px" pb={1} />
+          <Divider
+            orientation="vertical"
+            h="85%"
+            display={{ base: 'none', lg: 'block' }}
+          />
+          <Skeleton
+            w="10%"
+            h="20px"
+            pb={1}
+            display={{ base: 'none', lg: 'block' }}
+          />
+        </HStack>
+      </Flex>
     </Flex>
   </Flex>
 )
@@ -64,7 +103,7 @@ const ListAdsSkeleton = () => {
         <HomeIcon width="28px" height="28px" />
         <Skeleton ml={3} h="20px" w="50%" />
       </Flex>
-      <VStack spacing={6} py={6} w="100%" bg="white">
+      <VStack spacing={{ base: 3, md: 6 }} py={6} w="100%" bg="white">
         <AdCardSkeleton />
         <AdCardSkeleton />
         <AdCardSkeleton />

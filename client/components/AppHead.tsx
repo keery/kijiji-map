7989cff2025fd00f85282d8baa36e8fd9f0d8310
Guillaume <contact@guillaumeesnault.fr>
2021-09-17 12:12:42 -0400

    feat: add seo tags

--- client/components/AppHead.tsx
+++ client/components/AppHead.tsx
@@ -1,35 +1,63 @@
 import React from 'react'
 import NextHead from 'next/head'
+import { useTranslation } from 'next-i18next'
+import { DefaultSeo } from 'next-seo'
 
 const AppHead = () => {
+  const { t } = useTranslation()
   return (
-    <NextHead>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/assets/favicon/apple-touch-icon.png"
+    <>
+      <NextHead>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/assets/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/assets/favicon/safari-pinned-tab.svg"
+          color="#373373"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </NextHead>
+      <DefaultSeo
+        titleTemplate="%s | Jikiki"
+        defaultTitle={t('seo.title')}
+        description={t('seo.description')}
+        openGraph={{
+          type: 'website',
+          url: process.env.NEXT_PUBLIC_URL,
+          site_name: 'Jikiki',
+          images: [
+            {
+              url: process.env.NEXT_PUBLIC_URL + '/assets/img/jikiki-logo.png',
+              width: 100,
+              height: 52,
+              alt: 'Jikiki logo',
+            },
+          ],
+        }}
+        twitter={{
+          handle: 'jikiki',
+          site: process.env.NEXT_PUBLIC_URL,
+          cardType: 'summary_large_image',
+        }}
       />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/assets/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/assets/favicon/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/assets/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/assets/favicon/safari-pinned-tab.svg"
-        color="#373373"
-      />
-      <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#ffffff" />
-    </NextHead>
+    </>
   )
 }
 

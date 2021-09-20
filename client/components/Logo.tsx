2a3b818dd92ee8799c39d48f7fdbeb45dae43d61
Guillaume <contact@guillaumeesnault.fr>
2021-09-20 11:41:41 -0400

    style: size logo

--- client/components/Logo.tsx
+++ client/components/Logo.tsx
@@ -4,9 +4,9 @@ import Couch from 'public/assets/img/jikiki-logo.svg'
 
 const Logo = () => {
   return (
-    <Flex transform="translateY(2px)" minW="80px">
-      <Link href="/">
-        <Couch width="100px" />
+    <Flex transform="translateY(2px)" minW="80px" maxW="100px" mr={2}>
+      <Link href="/" display="inline-block">
+        <Couch width="100%" />
       </Link>
     </Flex>
   )
